39c8af90f592230f7cc22a0f446c14bfae563052
Zayn <zaynmuhammad4@hotmail.com>
2021-09-15 19:04:34 -0400

    Finished basic layout of intro for site

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ function watchTask() {
       "sass-boilerplate/stylesheets/layout/_main-content.scss",
       "sass-boilerplate/stylesheets/layout/_footer.scss",
       "sass-boilerplate/stylesheets/layout/_header_desktop.scss",
+      "sass-boilerplate/stylesheets/layout/_main-content_desktop.scss",
     ],
     buildStyles
   );
